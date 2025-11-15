d74dc0a5c28440b5e52e5f2c64327b0d3d22e918
Sylsk <130169759+Sylsk@users.noreply.github.com>
2025-11-14 22:42:54 -0300

    Cambios para que funcione el modulo de compras

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,6 @@ async function bootstrap() {
     credentials: true,
   });
    app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3004);
 }
 bootstrap();
